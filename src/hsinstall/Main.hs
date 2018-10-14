4a2af435167f77bb7eeb4dcb9bbebffbb56620d1
Dino Morelli <dino@ui3.info>
2018-10-14 12:43:41 -0400

    Replaced recursive dir copy code with a stock function
    This (3-function-long) block of code was replaced with a single call to
    Distribution.Simple.Utils.copyDirectoryRecursive

--- src/hsinstall/Main.hs
+++ src/hsinstall/Main.hs
@@ -4,7 +4,8 @@ import Data.Version ( showVersion )
 import Distribution.Package
 import Distribution.PackageDescription hiding ( options )
 import Distribution.PackageDescription.Parse
-import Distribution.Verbosity
+import Distribution.Simple.Utils ( copyDirectoryRecursive )
+import Distribution.Verbosity ( normal, silent, verbose )
 import Paths_hsinstall ( version )
 import System.Console.GetOpt
 import System.Directory
@@ -208,44 +209,6 @@ formattedVersion = do
   return $ printf "%s %s" progName (showVersion version)
 
 
-{-
-   Recursive file copying code
-
-   It was desireable to have a standalone recursive file copy in
-   this script for maximum cross-platform compatibility and to
-   avoid Haskell library dependencies.
-
-   Many thanks to [abuzittin gillifirca](https://codereview.stackexchange.com/users/20251/abuzittin-gillifirca) for the StackOverflow post [Copying files in Haskell](https://codereview.stackexchange.com/questions/68908/copying-files-in-haskell) where the following code was lifted.
--}
-
 copyTree :: Bool -> FilePath -> FilePath -> IO ()
-copyTree chatty s t = do
-    createDirectoryIfMissing True t
-    subItems <- getSubitems s
-    mapM_ (copyItem chatty s t) subItems
-
-
-getSubitems :: FilePath -> IO [(Bool, FilePath)]
-getSubitems path = getSubitems' ""
-  where
-    getChildren path' =  (\\ [".", ".."]) <$> getDirectoryContents path'
-
-    getSubitems' relPath = do
-        let absPath = path </> relPath
-        isDir <- doesDirectoryExist absPath
-        children <- if isDir then getChildren absPath else return []
-        let relChildren = [relPath </> p | p <- children]
-        ((isDir, relPath) :) . concat <$> mapM getSubitems' relChildren
-
-
-copyItem :: Bool -> FilePath -> FilePath -> (Bool, FilePath) -> IO ()
-copyItem chatty baseSourcePath baseTargetPath (isDir, relativePath) = do
-    let sourcePath = baseSourcePath </> relativePath
-    let targetPath = baseTargetPath </> relativePath
-
-    when chatty $
-       putStrLn $ "Copying " ++ sourcePath ++ " to " ++ targetPath
-
-    if isDir
-      then createDirectoryIfMissing False targetPath
-      else copyFile sourcePath targetPath
+copyTree True  source target = copyDirectoryRecursive verbose source target
+copyTree False source target = copyDirectoryRecursive silent  source target
