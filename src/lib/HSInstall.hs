cd51bd50469c96774f333f9b3fbc662674092a23
Dino Morelli <dino@ui3.info>
2018-10-18 15:47:53 -0400

    Removed bundle and no-version behavior from HSInstall library
    Closes gh-15

--- src/lib/HSInstall.hs
+++ src/lib/HSInstall.hs
@@ -8,7 +8,6 @@ module HSInstall
    ( getRsrcDir )
    where
 
-import Control.Monad ( liftM2, mplus )
 import System.Directory ( doesDirectoryExist )
 import System.Environment ( getExecutablePath )
 import System.FilePath ( (</>), takeDirectory, takeFileName )
@@ -30,41 +29,12 @@ import System.FilePath ( (</>), takeDirectory, takeFileName )
    @
 -}
 getRsrcDir :: IO FilePath -> IO FilePath
-getRsrcDir cabalDataDir =
-   maybe (fail "Unable to find resources directory")
-      return =<< searchResult
+getRsrcDir cabalDataDir = do
+  appDir <- takeFileName <$> cabalDataDir
+  rsrcPath <- ( </> "share" </> appDir </> "resources" )
+    . takeDirectory . takeDirectory <$> getExecutablePath
 
-   where
-      searchResult :: IO (Maybe FilePath)
-      searchResult = foldl (liftM2 mplus) (return Nothing) $ map (>>= mbExists)
-         [ mkRsrcPathFHS cabalDataDir
-         , mkRsrcPathFHSNoVer cabalDataDir
-         , mkRsrcPathBundle
-         ]
-
-      mbExists :: FilePath -> IO (Maybe FilePath)
-      mbExists p = do
-         exists <- doesDirectoryExist p
-         return $ if exists then Just p else Nothing
-
-
-mkRsrcPathFHS :: IO FilePath -> IO FilePath
-mkRsrcPathFHS cabalDataDir = do
-   appDir <- takeFileName <$> cabalDataDir
-   ( </> "share" </> appDir </> "resources" ) . takeDirectory . takeDirectory
-      <$> getExecutablePath
-
-
-mkRsrcPathFHSNoVer :: IO FilePath -> IO FilePath
-mkRsrcPathFHSNoVer cabalDataDir = do
-   appDir <- takeFileName <$> cabalDataDir
-   ( </> "share" </> (removeVersion appDir) </> "resources" ) . takeDirectory . takeDirectory
-      <$> getExecutablePath
-
-   where
-      removeVersion = init . reverse . dropWhile (/= '-') . reverse
-
-
-mkRsrcPathBundle :: IO FilePath
-mkRsrcPathBundle =
-   ( </> "resources" ) . takeDirectory . takeDirectory <$> getExecutablePath
+  rsrcPathExists <- doesDirectoryExist rsrcPath
+  if rsrcPathExists
+    then return rsrcPath
+    else fail "Unable to find resources directory"
