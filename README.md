861cdad7b8c6708e576b8f8b757f23b871d6a769
Dino Morelli <dino@ui3.info>
2018-10-22 07:45:27 -0400

    Made some adjustments to the README.md and usage

--- README.md
+++ README.md
@@ -18,8 +18,8 @@ command and adds more features. Those are:
 - Copying the `resources` directory into the deployment directory so these
   files can be located using relative paths at runtime (more on this later in
   RESOURCES)
-- Building the AppDir directory structure for a project and producing an
-  AppImage
+- Building an AppDir directory structure for a project and producing an
+  [AppImage](https://appimage.org/)
 
 It will be necessary to have the Haskell stack tool on your PATH:  
 https://docs.haskellstack.org/en/stable/README/
@@ -36,11 +36,11 @@ the project will be deployed to `AppDir/usr/bin`.
 
 The second mode is intended to set up for AppImage creation and is triggered by
 specifying exactly one EXECUTABLE from the project in the arguments. This will
-change the PREFIX to `AppDir_EXECUTABLE/usr`. And ONLY that single executable
-will be copied to this `AppDir_EXECUTABLE/usr/bin` directory.
+change the PREFIX to `AppDir_EXECUTABLE/usr`. And only that single executable
+will be copied to the `AppDir_EXECUTABLE/usr/bin` directory.
 
-The directory layout will be a traditional UNIX structure, also known as the
-FHS. Like this:
+The directory layout will be a standard [FHS](http://www.pathname.com/fhs/)
+shape, common in UNIX-like operating systems. Like this:
 
     <PREFIX>/
       bin/...
@@ -49,7 +49,7 @@ FHS. Like this:
           doc/LICENSE
           resources/...
 
-Be aware that when the --delete switch is used the binaries in `<PREFIX>/bin`
+Be aware that when the `--delete` switch is used the binaries in `<PREFIX>/bin`
 WILL NOT be deleted, only the share directory:
 `<PREFIX>/share/<PROJECT>-<VERSION>`
 
@@ -57,8 +57,8 @@ WILL NOT be deleted, only the share directory:
 
 Even for a first-time AppImaging, this tool should produce a working AppImage.
 If missing, it will create default `.desktop` and `.svg` files in
-`util/resources/appimage`. Customize these to fit your project, and then check
-these two files into source control for future builds.
+`util/resources/appimage`. Customize or replace these to fit your project, and
+then check these two files into source control for future builds.
 
 The default `.desktop` file Categories will be populated with 'Utility;'. We
 recommend adjusting this using the XDG list of registered categories:
--- src/app/HSInstall/Opts.hs
+++ src/app/HSInstall/Opts.hs
@@ -100,7 +100,7 @@ hsinstall is a tool for deploying software projects into directory structures su
 
 - Copying the `LICENSE` file into the deployment directory
 - Copying the `resources` directory into the deployment directory so these files can be located using relative paths at runtime (more on this later in RESOURCES)
-- Building the AppDir directory structure for a project and producing an AppImage
+- Building an AppDir directory structure for a project and producing an AppImage
 
 It will be necessary to have the Haskell stack tool on your PATH:
 https://docs.haskellstack.org/en/stable/README/
@@ -113,9 +113,9 @@ MODES
 
 hsinstall operates in two modes. The first is a plain deployment with no AppImage creation. The PREFIX will default to `AppDir/usr` and all binaries in the project will be deployed to `AppDir/usr/bin`.
 
-The second mode is intended to set up for AppImage creation and is triggered by specifying exactly one EXECUTABLE from the project in the arguments. This will change the PREFIX to `AppDir_EXECUTABLE/usr`. And ONLY that single executable will be copied to this `AppDir_EXECUTABLE/usr/bin` directory.
+The second mode is intended to set up for AppImage creation and is triggered by specifying exactly one EXECUTABLE from the project in the arguments. This will change the PREFIX to `AppDir_EXECUTABLE/usr`. And only that single executable will be copied to the `AppDir_EXECUTABLE/usr/bin` directory.
 
-The directory layout will be a traditional UNIX structure, also known as the FHS. Like this:
+The directory layout will be a standard FHS shape, common in UNIX-like operating systems. Like this:
 
     <PREFIX>/
       bin/...
@@ -124,11 +124,11 @@ The directory layout will be a traditional UNIX structure, also known as the FHS
           doc/LICENSE
           resources/...
 
-Be aware that when the --delete switch is used the binaries in `<PREFIX>/bin` WILL NOT be deleted, only the share directory: `<PREFIX>/share/<PROJECT>-<VERSION>`
+Be aware that when the `--delete` switch is used the binaries in `<PREFIX>/bin` WILL NOT be deleted, only the share directory: `<PREFIX>/share/<PROJECT>-<VERSION>`
 
 APPIMAGE CREATION
 
-Even for a first-time AppImaging, this tool should produce a working AppImage. If missing, it will create default `.desktop` and `.svg` files in `util/resources/appimage`. Customize these to fit your project, and then check these two files into source control for future builds.
+Even for a first-time AppImaging, this tool should produce a working AppImage. If missing, it will create default `.desktop` and `.svg` files in `util/resources/appimage`. Customize or replace these to fit your project, and then check these two files into source control for future builds.
 
 The default `.desktop` file Categories will be populated with 'Utility;'. We recommend adjusting this using the XDG list of registered categories: https://specifications.freedesktop.org/menu-spec/latest/apa.html
 
